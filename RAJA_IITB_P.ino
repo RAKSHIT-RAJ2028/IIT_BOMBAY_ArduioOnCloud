e47363880bde780e51356e3609253a6bab6534a1
RAKSHIT-RAJ2028 <131436298+RAKSHIT-RAJ2028@users.noreply.github.com>
2023-04-23 08:21:34 +0530

    Add files via upload
    RAJA_IITB_P is the file where  Task 1: Incorporating new components in the Platform is completed by using DHT11 SENSOR and  ESP8266 (Nodemcu)

---
+++ RAJA_IITB_P.ino
@@ -0,0 +1,33 @@
+#include <DHT.h>
+#include<ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include<WiFiClient.h>
+#define DHTTYPE DHT11
+uint8_t DHTPIN = D1;
+
+DHT mydht(DHTPIN,DHTTYPE);
+const char* ssid = "Mahadev";
+const char* password = "bholenath";
+float humidity, temp;
+
+void setup() {
+
+  Serial.begin(9600);
+  //WiFi.begin(ssid,password);
+  pinMode(DHTPIN,INPUT);
+  mydht.begin();
+  delay(2000);
+   humidity = mydht.readHumidity();
+   temp = mydht.readTemperature();
+   delay(2000);
+}
+
+void loop() {
+  HTTPClient http;
+  WiFiClient Client;
+ humidity = mydht.readHumidity();
+ temp = mydht.readTemperature();
+ Serial.print("Humidity :");Serial.println(humidity);
+ Serial.print("Temp: ");Serial.println(temp);
+delay(1000);
+}
